feab51a438bffce0ffee89afa4e5624020d16276
andrii <anazrr@outlook.com>
2021-02-12 14:41:17 +0000

    my lab edits

---
+++ Labs/07/Starter/func/FileParser.cs
@@ -0,0 +1,21 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public static class FileParser
+{
+    [FunctionName("FileParser")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger("GET")] HttpRequest request)
+    {
+        // string connectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
+        // BlobClient blob = new BlobClient(connectionString, "drop", "records.json");
+
+        BlobClient blob = new BlobClient(new Uri("https://securestorak07.blob.core.windows.net/drop/records.json"));
+        var response = await blob.DownloadAsync();
+        return new FileStreamResult(response?.Value?.Content, response?.Value?.ContentType);
+    }
+}
